0c2224fb0257264bd5223ca7ef24ec58476afb6a
ProgramerPlus <hipitelo@outlook.com>
2025-09-19 22:45:09 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("We Will Rock You\n");
+Console.ResetColor();
+
+Console.WriteLine("Buddy, you're a boy, make a big noise");
+Console.WriteLine("Playing in the street, gonna be a big man someday");
+Console.WriteLine("You got mud on your face, you big disgrace");
+Console.WriteLine("Kicking your can all over the place\n");
+Console.ForegroundColor = (ConsoleColor)11;
+Console.WriteLine("Singing \nWe will, we will rock you \nWe will, we will rock you\n");
+Console.ResetColor();
+Console.WriteLine("Buddy, you're a young man, hard man");
+Console.WriteLine("Shouting in the street, gonna take on the world someday");
+Console.WriteLine("You got blood on your face, you big disgrace");
+Console.WriteLine("Waving your banner all over the place\n");
+Console.ForegroundColor = (ConsoleColor)11;
+Console.WriteLine("We will, we will rock you \nSing it! \nWe will, we will rock you\n");
+Console.ResetColor();
+Console.WriteLine("Buddy, you're an old man, poor man");
+Console.WriteLine("Pleading with your eyes, gonna make you some peace someday");
+Console.WriteLine("You got mud on your face, you big disgrace");
+Console.WriteLine("Somebody better put you back into your place\n");
+
+Console.ForegroundColor = (ConsoleColor)11;
+Console.WriteLine("We will, we will rock you \nSing it! \nWe will, we will rock you\n");
+Console.WriteLine("Everybody \nWe will, we will rock you \nWe will, we will rock you");
+Console.ResetColor();
